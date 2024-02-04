652103b2a41050a0915a288f0c31709abb5f1b46
SergioEanX <sergio.ferlito@enea.it>
2024-02-04 12:15:26 +0100

    Initial todo component

---
+++ vue3-crash-app/src/components/TodoHeader.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<header>
+  <nav class="container">
+    <div class="branding">
+      <img src="../assets/Vue_Logo_Black.png" alt="VUE 3 Logo">
+      <h1>Vue Todos</h1>
+    </div>
+    <ul class="nav-routes">
+      <RouterLink to="/">
+Home
+      </RouterLink>
+      <router-link to="/about">About</router-link>
+    </ul>
+
+  </nav>
+</header>
+</template>
+
+<style scoped>
+
+</style>
