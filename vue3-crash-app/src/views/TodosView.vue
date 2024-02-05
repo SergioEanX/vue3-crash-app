b32ed56bda5be78e25797b574087b5f5f529199b
SergioEanX <sergio.ferlito@enea.it>
2024-02-05 10:23:11 +0100

    added toggleTodoComplete, toggleEditTodo,updateTodo

--- vue3-crash-app/src/views/TodosView.vue
+++ vue3-crash-app/src/views/TodosView.vue
@@ -5,16 +5,40 @@ import { uid } from 'uid'
 
 import { ref } from 'vue'
 import TodoItem from '@/components/TodoItem.vue'
-const todoList =ref([]);
-const createTodo =(todo)=>{
-  const item={
+
+
+type Todo = {
+  id:string;
+  todo: string;
+  isCompleted: boolean|null; // Check for this property
+  isEditing: boolean|null;
+};
+
+
+const todoList =ref<Todo[]>([]);
+const createTodo =(todo:string)=>{
+  const item:Todo={
     id: uid(),
     todo,
     isCompleted:null,
     isEditing:null
   }
-
   todoList.value.push(item)
+}
+
+const toggleTodoComplete = (todoPos:number)=>{
+
+todoList.value[todoPos].isCompleted = !todoList.value[todoPos].isCompleted;
+
+}
+
+const toggleEditTodo =(todoPos:number) =>{
+  todoList.value[todoPos].isEditing = !todoList.value[todoPos].isEditing;
+
+}
+
+const updateTodo = (todoVal:string,todoPos:number) =>{
+todoList.value[todoPos].todo=todoVal
 
 }
 </script>
@@ -27,7 +51,11 @@ const createTodo =(todo)=>{
     <todo-create @create-todo="createTodo">
     </todo-create>
     <ul class="todo-list" v-if="todoList.length>0">
-      <todo-item v-for="item in todoList" :todo="item"/>
+      <todo-item v-for="(item, index) in todoList" :todo="item"
+                 :index="index"
+                 @toggle-complete="toggleTodoComplete"
+                 @update-todo="updateTodo"
+      @edit-todo="toggleEditTodo"/>
     </ul>
     <p class="todos-msg" v-else>
       <Icon icon="noto-v1:sad-but-relieved-face" />
